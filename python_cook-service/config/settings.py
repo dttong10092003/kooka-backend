1100607689ac3f1f8fd647f18db3dbe1b67642a3
DESKTOP-BPE6J3K\DELL <tong10092003@gmail.com>
2025-09-22 20:58:54 +0700

    Fix docker-compose

--- python_cook-service/config/settings.py
+++ python_cook-service/config/settings.py
@@ -1,12 +1,11 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     MONGODB_URI: str
-    DB_NAME: str
-    COLLECTION_NAME: str
-    CHROMA_PATH: str
+    DB_NAME: str = "test"
+    COLLECTION_NAME: str = "recipes"
+    CHROMA_PATH: str = "./chroma_db"
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 settings = Settings()
