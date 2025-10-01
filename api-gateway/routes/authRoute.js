ab27f597e177897e7de77bf5fd30d53b81f2dd57
tinph010803 <tinphan924@gmail.com>
2025-10-01 09:37:57 +0700

    add middleware to auth, user

--- api-gateway/routes/authRoute.js
+++ api-gateway/routes/authRoute.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const buildProxy = require("../utils/proxyFactory");
+const verifyToken = require("../middlewares/verifyToken");
+const isAdmin = require("../middlewares/isAdmin");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -10,8 +12,8 @@ const proxyAuthService = buildProxy(process.env.AUTH_SERVICE_URL);
 // Auth routes - proxy to auth-service
 router.post("/register", proxyAuthService);
 router.post("/login", proxyAuthService);
-router.post("/change-password", proxyAuthService);
-router.post("/create-admin", proxyAuthService);
+router.post("/change-password", verifyToken, proxyAuthService);
+router.post("/create-admin", verifyToken, isAdmin, proxyAuthService);
 
 // Google OAuth routes
 router.get("/google", proxyAuthService);
--- api-gateway/routes/userRoute.js
+++ api-gateway/routes/userRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const buildProxy = require("../utils/proxyFactory");
+const verifyToken = require("../middlewares/verifyToken");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -7,9 +8,9 @@ const router = express.Router();
 
 const proxyUserService = buildProxy(process.env.USER_SERVICE_URL);
 
-// Profile routes - proxy to user-service
-router.post("/profile", proxyUserService);
-router.get("/profile/:userId", proxyUserService);
-router.put("/profile/:userId", proxyUserService);
+// Profile routes - proxy to user-service (all protected routes)
+router.post("/profile", verifyToken, proxyUserService);
+router.get("/profile/:userId", verifyToken, proxyUserService);
+router.put("/profile/:userId", verifyToken, proxyUserService);
 
 module.exports = router;
