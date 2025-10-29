2a57bd648cdba01b7a737a5c8aeccfc4e1d9b7ad
tinph010803 <tinphan924@gmail.com>
2025-10-29 11:39:16 +0700

    update token len 3h

--- auth-service/src/services/authService.js
+++ auth-service/src/services/authService.js
@@ -51,7 +51,7 @@ function generateToken(user) {
   return jwt.sign(
     { id: user._id, username: user.username, email: user.email, isAdmin: user.isAdmin },
     process.env.JWT_SECRET,
-    { expiresIn: "1h" }
+    { expiresIn: "3h" }
   );
 }
 
